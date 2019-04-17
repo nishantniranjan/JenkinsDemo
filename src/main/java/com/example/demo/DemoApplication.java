85522da232c8350f800c55ba52ccb24cf7bac0b7
Nishant Niranjan <nishant.niranjan.tech@gmail.com>
2019-04-18 01:04:17 +0530

    third web commit

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,19 +1,24 @@
 package com.example.demo;
 
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class DemoApplication implements CommandLineRunner{
+public class DemoApplication extends SpringBootServletInitializer{
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
 
 	@Override
-	public void run(String... args) throws Exception {
-		System.out.println("Hi there-new updated");
-	}
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(DemoApplication.class);
+    }
+	/*
+	 * @Override public void run(String... args) throws Exception {
+	 * System.out.println("Hi there-new updated"); }
+	 */
 
 }
